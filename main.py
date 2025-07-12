b0ec1359692e994357a2c980073e9fa7ac44152a
CYHsuRHS <rhs.hsu@gmail.com>
2025-07-12 15:59:39 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import shopping
+
+app = FastAPI()
+
+@app.get("/")
+def read_main(): # async def read_main(): 
+    return {"msg": "Hello World"}
+
+def get_pricing(obj):
+    return obj["pricing"]
+
+
+@app.get("/pricing")
+def pricing(keyword: str, sort_by: str = None):
+    eslite_data = shopping.query_eslite(keyword)
+    pchome_data = shopping.query_pchome(keyword)
+    data = pchome_data + eslite_data
+    if sort_by == "-pricing":
+        data.sort(key=get_pricing)
+        # data = sorted(data, key=get_pricing)
+    if sort_by == "pricing":
+        data.sort(key=get_pricing, reverse=True)
+    # print("test")
+    return data
